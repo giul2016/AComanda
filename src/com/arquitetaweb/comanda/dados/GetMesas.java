915aba4054c830b6cc1c5305eec5fb9d1dcb4905
unknown <marcos.tomazini@gmail.com>
2014-03-02 14:38:05 -0300

    #020320141438

--- src/com/arquitetaweb/comanda/dados/GetMesas.java
+++ src/com/arquitetaweb/comanda/dados/GetMesas.java
@@ -4,10 +4,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.app.Fragment;
 import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
@@ -31,39 +33,86 @@ public class GetMesas {
 	GridView mesas;
 	public static MesaAdapter adapter;
 
-	ProgressDialog progressDialog;
-
 	public GetMesas(Fragment fragment) {
 		this.fragment = fragment;
 		this.context = fragment.getActivity();
-		this.view = fragment.getView();		
+		this.view = fragment.getView();
 	}
 
 	public void carregarMesas() {
-		try {
+		new CarregaMesa().execute();
+	}
+
+	private class CarregaMesa extends AsyncTask<String, Void, Boolean> {
+		protected ProgressDialog progressDialog;
+
+		@Override
+		protected void onPreExecute() {
+			super.onPreExecute();
 			progressDialog = new ProgressDialog(context);
 			progressDialog.setCancelable(false);
 			progressDialog.setMessage("atualizando mapa das mesas...");
 			progressDialog.show();
+		}
 
-			Thread thread = new Thread() {
-				public void run() {
-					try {
-						if (Utils.isConnected(context)) {
-							getMesas();
-						} else {
-							errorConnectServer();
-						}
-					} finally {
-						if (progressDialog != null)
-							progressDialog.dismiss();
+		@Override
+		protected Boolean doInBackground(String... params) {
+			if (Utils.isConnected(context)) {
+				String urlApi = Utils.getUrlServico(context)
+						+ "/Api/SituacaoMesas";
+				JSONParser jParser = new JSONParser();
+				final String jsonMesas = jParser.getJSONFromApi(urlApi);
+				((Activity) context).runOnUiThread(new Runnable() {
+					public void run() {
+						List<MesaModel> mesasLista = new ArrayList<MesaModel>();
+						Gson gson = new Gson();
+						mesasLista = gson.fromJson(jsonMesas,
+								new MesaModel().getType()); // converte pra
+															// ArrayList de
+															// mesas
+
+						mesas = (GridView) view.findViewById(R.id.list);
+						adapter = new MesaAdapter((Activity) context,
+								mesasLista);
+						mesas.setAdapter(adapter);
+
+						// Click event for single list row
+						mesas.setOnItemClickListener(new OnItemClickListener() {
+							@Override
+							public void onItemClick(AdapterView<?> parent,
+									View view, int position, long id) {
+
+								abrirDetalhes(view, position);
+							}
+
+							private void abrirDetalhes(View view, Integer idMesa) {
+								Intent intent = new Intent(view.getContext(),
+										DetailsActivity.class);
+
+								MesaModel mesaObj = adapter.getItem(idMesa);
+								String mesaGson = new Gson().toJson(mesaObj);
+								intent.putExtra("mesa", mesaGson);
+								fragment.startActivityForResult(intent, 100);
+							}
+						});
 					}
-				}
-			};
-			thread.start();
-		} catch (Exception e) {
-			if (progressDialog != null)
-				progressDialog.dismiss();
+				});
+			} else {
+				errorConnectServer();
+			}
+			return true;
+		}
+
+		@Override
+		protected void onPostExecute(Boolean result) {
+			progressDialog.dismiss();
+			super.onPostExecute(result);
+		}
+
+		@Override
+		protected void onCancelled() {
+			// TODO Auto-generated method stub
+			super.onCancelled();
 		}
 	}
 
@@ -83,7 +132,11 @@ public class GetMesas {
 			public void run() {
 				List<MesaModel> mesasLista = new ArrayList<MesaModel>();
 				Gson gson = new Gson();
-				mesasLista = gson.fromJson(jsonMesas, new MesaModel().getType()); // converte pra ArrayList de mesas
+				mesasLista = gson.fromJson(jsonMesas, new MesaModel().getType()); // converte
+																					// pra
+																					// ArrayList
+																					// de
+																					// mesas
 
 				mesas = (GridView) view.findViewById(R.id.list);
 				adapter = new MesaAdapter((Activity) context, mesasLista);
@@ -101,7 +154,7 @@ public class GetMesas {
 					private void abrirDetalhes(View view, Integer idMesa) {
 						Intent intent = new Intent(view.getContext(),
 								DetailsActivity.class);
- 
+
 						MesaModel mesaObj = adapter.getItem(idMesa);
 						String mesaGson = new Gson().toJson(mesaObj);
 						intent.putExtra("mesa", mesaGson);
@@ -113,7 +166,6 @@ public class GetMesas {
 	}
 
 	private void errorConnectServer() {
-		// progressDialog.dismiss();
 		((Activity) context).runOnUiThread(new Runnable() {
 			public void run() {
 				new Alerta()
